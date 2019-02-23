559c3aa5535a85c1bf09bfbc06bdb7468f2936b8
faizahmed1314 <foyezahmedfj20@gmail.com>
2019-02-23 13:52:25 +0600

    initail commit

---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your name");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("Hello"+ name);
+            Console.ReadKey();
+            
+        }
+    }
+}
